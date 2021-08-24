9e1fe1d2a94a544a2a06decad22ba81932fb30ad
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-08-24 10:59:28 +0430

    Added ChakraProvider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-function App() {
-  return null;
-}
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const App = () => {
+  const theme = extendTheme({ direction: "rtl" });
+  return <ChakraProvider theme={theme}>ok</ChakraProvider>;
+};
 
 export default App;
