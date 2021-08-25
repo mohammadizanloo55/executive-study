af1c65c283d148ae5cdc631bd0ddc13c804495ad
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-08-25 17:22:56 +0430

    Added Header to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { lazy, Suspense } from "react";
+
+import Loading from "./Components/Loading/Loading";
+
+const Header = lazy(() => import("./Components/Header/Header"));
 
 const App = () => {
   const theme = extendTheme({ direction: "rtl" });
-  return <ChakraProvider theme={theme}>ok</ChakraProvider>;
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Suspense fallback={<Loading />}>
+        <Header />
+      </Suspense>
+    </ChakraProvider>
+  );
 };
 
 export default App;
