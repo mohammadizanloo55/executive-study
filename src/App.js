362893795943ecc8ff9387178d1bb1259d9aae78
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-08-30 17:26:47 +0430

    Added Detail Component to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, Container, extendTheme } from "@chakra-ui/react";
 import { lazy, Suspense } from "react";
 
 import Loading from "./Components/Loading/Loading";
 
 const Header = lazy(() => import("./Components/Header/Header"));
-
+const Detail = lazy(() => import("./Components/Detail/Detail"));
 const App = () => {
   const theme = extendTheme({ direction: "rtl" });
 
   return (
     <ChakraProvider theme={theme}>
       <Suspense fallback={<Loading />}>
-        <Header />
+        <Container maxW="8xl">
+          <Header />
+          <Detail />
+        </Container>
       </Suspense>
     </ChakraProvider>
   );
