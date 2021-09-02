361d5978eb372b388387f4a9b2628c251d3e2344
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-09-02 11:11:46 +0430

    Moved useBreakpointValue to CustomSwiper Component for better performance

--- src/Components/CustomSwiper/CustomSwiper.js
+++ src/Components/CustomSwiper/CustomSwiper.js
@@ -2,28 +2,35 @@ import "swiper/swiper.min.css";
 import "swiper/swiper-bundle.min.css";
 import "swiper/components/pagination/pagination.min.css";
 
+import { useBreakpointValue } from "@chakra-ui/react";
 import SwiperCore, { Autoplay, Lazy, Pagination } from "swiper";
 import { Swiper } from "swiper/react";
 
 SwiperCore.use([Pagination, Autoplay, Lazy]);
-const CustomSwiper = ({ SlidesPerView, Style, ...props }) => (
-  <Swiper
-    loop={true}
-    preloadImages={false}
-    lazy={true}
-    autoplay={{
-      delay: 1500,
-      disableOnInteraction: false,
-      pauseOnMouseEnter: true,
-      waitForTransition: true,
-    }}
-    onMouseOut={() => console.log("da")}
-    spaceBetween={50}
-    slidesPerView={SlidesPerView}
-    style={Style}
-    pagination={true}
-  >
-    {props.children}
-  </Swiper>
-);
+const CustomSwiper = ({ SlidesPerView, Style, ...props }) => {
+  const ResponsiveSlidesPerView = useBreakpointValue([...SlidesPerView]);
+  if (!ResponsiveSlidesPerView) {
+    return null;
+  }
+  return (
+    <Swiper
+      loop={true}
+      preloadImages={false}
+      lazy={true}
+      autoplay={{
+        delay: 1500,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+        waitForTransition: true,
+      }}
+      onMouseOut={() => console.log("da")}
+      spaceBetween={50}
+      slidesPerView={ResponsiveSlidesPerView}
+      style={Style}
+      pagination={true}
+    >
+      {props.children}
+    </Swiper>
+  );
+};
 export default CustomSwiper;
